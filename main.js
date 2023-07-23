4aa4ea28f4e457f8a57b989760becba1fb37cfde
yashrajxchopra <yashrajxchopra@gmail.com>
2023-07-23 10:23:37 +0530

    Added Nav Bar

---
+++ main.js
@@ -0,0 +1,28 @@
+//Interpreted lang
+console.log('Hello')
+
+name = 'King King'
+console.log(name) 
+
+let myArr = [10, 20, 30, 40, 50]
+console.log(myArr)
+myArr.pop();
+console.log(myArr)
+
+let twoDarray= [ [10,20,30], [40,50,60], [70,80]]
+console.log(twoDarray[1][2])
+
+let myObj =
+{
+    name : 'username',
+    hobbies: ['Music', 'Gaming','movies'],
+    address:
+    {
+        street: 'Street-2',
+        city: 'Kharar',
+        pincodes:[1234, 5678],
+    }
+
+
+}
+console.log(myObj.address.pincodes[0])
